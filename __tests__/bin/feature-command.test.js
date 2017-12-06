b666a3b3ffd4c542784ef34aa36b3ba24cf47f7b
UnleashedMind <michaelzhou.net@gmail.com>
2017-12-05 18:29:41 -0800

    add unit tests

---
+++ __tests__/bin/feature-command.test.js
@@ -0,0 +1,118 @@
+jest.dontMock('fs-extra')
+jest.mock('../../lib/project-info-manager.js')
+jest.mock('../../lib/utils/awsmobilejs-path-manager.js')
+jest.mock('../../lib/backend-operations/backend-spec-manager.js')
+
+const path = require('path')
+
+const featureCommand = require('../../bin/feature-command.js')
+
+const projectInfoManager = require('../../lib/project-info-manager.js')
+const pathManager = require('../../lib/utils/awsmobilejs-path-manager.js')
+const backendSpecManager = require('../../lib/backend-operations/backend-spec-manager.js')
+
+describe('run feature commands', () => {
+    const mockProjectInfo = {}
+    const mockOpsFeatureFilePath = path.normalize(path.join(__dirname, '../../__mocks__/mock-ops-feature.js'))
+    const mockOpsFeature = require(mockOpsFeatureFilePath)
+
+    beforeAll(() => {
+        global.console = {log: jest.fn()}
+        
+        projectInfoManager.getProjectInfo = jest.fn(()=>{
+            return mockProjectInfo
+        })
+
+        pathManager.getOpsFeatureFilePath = jest.fn(()=>{
+            return mockOpsFeatureFilePath
+        })
+
+        backendSpecManager.enableFeature = jest.fn()
+        backendSpecManager.disableFeature = jest.fn()
+        backendSpecManager.configureFeature = jest.fn()
+    })
+
+    beforeEach(() => {
+        backendSpecManager.enableFeature.mockClear()
+        backendSpecManager.disableFeature.mockClear()
+        backendSpecManager.configureFeature.mockClear()
+        mockOpsFeature.runCommand.mockClear()
+    })
+
+    test('enable', () => {
+       const args = ['node', 'awsmobile', mockOpsFeature.featureName, 'enable']
+       featureCommand.run(mockOpsFeature.featureName, args)
+
+        expect(backendSpecManager.enableFeature).toBeCalled()
+        expect(backendSpecManager.enableFeature.mock.calls[0][0]).toBe(mockProjectInfo)
+        expect(backendSpecManager.enableFeature.mock.calls[0][1]).toBe(mockOpsFeature.featureName)
+        expect(backendSpecManager.enableFeature.mock.calls[0][2]).toBe(false)
+    })
+    
+    test('enable with prompt', () => {
+        const args = ['node', 'awsmobile', mockOpsFeature.featureName, 'enable', '--prompt']
+        featureCommand.run(mockOpsFeature.featureName, args)
+ 
+         expect(backendSpecManager.enableFeature).toBeCalled()
+         expect(backendSpecManager.enableFeature.mock.calls[0][0]).toBe(mockProjectInfo)
+         expect(backendSpecManager.enableFeature.mock.calls[0][1]).toBe(mockOpsFeature.featureName)
+         expect(backendSpecManager.enableFeature.mock.calls[0][2]).toBe(true)
+     })
+     
+     test('enable with none existing option', () => {
+         const args = ['node', 'awsmobile', mockOpsFeature.featureName, 'enable', '--nonExistingOption']
+         featureCommand.run(mockOpsFeature.featureName, args)
+  
+          expect(backendSpecManager.enableFeature).not.toBeCalled()
+    })
+
+    test('disable', () => {
+       const args = ['node', 'awsmobile', mockOpsFeature.featureName, 'disable']
+       featureCommand.run(mockOpsFeature.featureName, args)
+
+        expect(backendSpecManager.disableFeature).toBeCalled()
+        expect(backendSpecManager.disableFeature.mock.calls[0][0]).toBe(mockProjectInfo)
+        expect(backendSpecManager.disableFeature.mock.calls[0][1]).toBe(mockOpsFeature.featureName)
+    })
+    
+    test('disable with none existing option', () => {
+        const args = ['node', 'awsmobile', mockOpsFeature.featureName, 'disable', '--nonExistingOption']
+        featureCommand.run(mockOpsFeature.featureName, args)
+
+        expect(backendSpecManager.disableFeature).not.toBeCalled()
+    })
+    
+    test('configure', () => {
+        const args = ['node', 'awsmobile', mockOpsFeature.featureName, 'configure']
+        featureCommand.run(mockOpsFeature.featureName, args)
+
+        expect(backendSpecManager.configureFeature).toBeCalled()
+        expect(backendSpecManager.configureFeature.mock.calls[0][0]).toBe(mockProjectInfo)
+        expect(backendSpecManager.configureFeature.mock.calls[0][1]).toBe(mockOpsFeature.featureName)
+    })
+    
+    test('configure with none existing option', () => {
+        const args = ['node', 'awsmobile', mockOpsFeature.featureName, 'configure', '--nonExistingOption']
+        featureCommand.run(mockOpsFeature.featureName, args)
+
+        expect(backendSpecManager.configureFeature).not.toBeCalled()
+    })
+    
+    test('feature specific command', () => {
+        const featureSpecificCommand = Object.keys(mockOpsFeature.featureCommands)[0]
+        const args = ['node', 'awsmobile', mockOpsFeature.featureName, featureSpecificCommand]
+        featureCommand.run(mockOpsFeature.featureName, args)
+
+        expect(mockOpsFeature.runCommand).toBeCalled()
+        expect(mockOpsFeature.runCommand.mock.calls[0][0]).toBe(featureSpecificCommand)
+        expect(mockOpsFeature.runCommand.mock.calls[0][1]).toBe(mockProjectInfo)
+        expect(mockOpsFeature.runCommand.mock.calls[0][2]).toBe(args)
+    })
+    
+    test('none existing command', () => {
+        const args = ['node', 'awsmobile', mockOpsFeature.featureName, 'nonExistingCommand']
+        featureCommand.run(mockOpsFeature.featureName, args)
+
+        expect(mockOpsFeature.runCommand).not.toBeCalled()
+    })
+})
---
+++ __tests__/lib/project-info-manager.test.js
@@ -0,0 +1,124 @@
+jest.mock('fs-extra')
+
+const fs = require('fs-extra')
+const path = require('path')
+const mockirer = require('mockirer')
+const inquirer = require('inquirer')
+
+const projectInfoManager = require('../../lib/project-info-manager.js')
+
+const awsmobileJSConstant = require('../../lib/utils/awsmobilejs-constant.js')
+const templateValidator = require('../../lib/project-validator.js')
+const pathManager = require('../../lib/utils/awsmobilejs-path-manager.js')
+const backendFormats = require('../../lib/backend-operations/backend-formats.js')
+
+describe('project info manager functions', () => {
+    const projectName = 'projectName'
+    const projectPath = path.join('/', projectName)
+    const projectInfoFilePath = pathManager.getProjectInfoFilePath(projectPath)
+    const backendYmlFilePath = pathManager.getBackendSpecProjectYmlFilePath(projectPath)
+    
+    const mock_projectInfo = {}
+    var MOCK_FILE_INFO = {}
+    MOCK_FILE_INFO[projectInfoFilePath] = JSON.stringify(mock_projectInfo, null, '\t')
+    MOCK_FILE_INFO[backendYmlFilePath] = JSON.stringify('--- !com.amazonaws.mobilehub.v0.Project', null, '\t')
+
+    const mock_srcDir = '/src'
+    const mock_distDir = '/dist'
+    const mock_buildCommand = 'npm run build'
+    const mock_startCommand = 'npm run start'
+
+    const mock_backendProjectDetails = {
+        "name": "name",
+        "projectId": "projectId",
+        "createdDate": "createdDate",
+        "lastUpdatedDate": "lastUpdatedDate",
+        "consoleUrl": "consoleUrl",
+        "InitializationTime": "",
+        "LastConfigurationTime": "",
+        "LastNPMInstallTime": "",
+        "FrontendLastBuildTime": "",
+        "LastPublishTime": "",
+        "BackendFormat": "yml",
+        "BackendLastBuildTime": "",
+        "BackendLastUpdateTime": "",
+        "BackendLastUpdateSuccessful": false,
+        "BackendProjectID": "",
+        "BackendProjectName":  "",
+        "BackendProjectConsoleUrl": "",
+        "BackendProjectCreationTime": "",
+        "BackendProjectLastUpdatedTime": ""
+    }
+
+    beforeAll(() => {
+        global.console = {log: jest.fn()}
+        process.cwd = jest.fn(()=>{ return projectPath })
+        fs.__setMockFiles(MOCK_FILE_INFO) 
+        mockirer(inquirer, {
+            srcDir: mock_srcDir, 
+            distDir: mock_distDir,
+            buildCommand: mock_buildCommand,
+            startCommand: mock_startCommand
+        }) 
+    })
+
+    beforeEach(() => {
+        fs.writeFileSync.mockClear()
+    })
+
+    test('initialize', () => {
+        projectInfoManager.initialize(projectPath)
+
+        expect(fs.writeFileSync).toBeCalled()
+        expect(fs.writeFileSync.mock.calls[0][0]).toBe(projectInfoFilePath)
+    })
+
+    test('configureProjectInfo', () => {
+       // process.cwd = jest.fn(()=>{ return projectPath })
+        const callback = jest.fn()
+        projectInfoManager.configureProjectInfo(callback)
+
+        expect(callback).toBeCalled()
+        expect(callback.mock.calls[0][1].SourceDir).toBe(mock_srcDir)
+        expect(callback.mock.calls[0][1].DistributionDir).toBe(mock_distDir)
+        expect(callback.mock.calls[0][1].BuildCommand).toBe(mock_buildCommand)
+        expect(callback.mock.calls[0][1].StartCommand).toBe(mock_startCommand)
+    })
+
+    test('getProjectInfo', () => {
+        //process.cwd = jest.fn(()=>{ return projectPath })
+      
+        const projectInfo = projectInfoManager.getProjectInfo()
+
+        expect(projectInfo.ProjectPath).toBe(projectPath)
+     })
+
+
+    test('setProjectInfo', () => {
+        // process.cwd = jest.fn(()=>{ return projectPath })
+        
+        const projectInfo = projectInfoManager.setProjectInfo(mock_projectInfo)
+
+        expect(fs.writeFileSync).toBeCalled()
+        expect(fs.writeFileSync.mock.calls[0][0]).toBe(projectInfoFilePath)
+    })
+
+    test('updateBackendProjectDetails', () => {
+        // process.cwd = jest.fn(()=>{ return projectPath })
+        
+        projectInfo = projectInfoManager.updateBackendProjectDetails(mock_projectInfo, mock_backendProjectDetails)
+
+        expect(fs.writeFileSync).toBeCalled()
+        expect(fs.writeFileSync.mock.calls[0][0]).toBe(projectInfoFilePath)
+        expect(projectInfo.BackendProjectID).toEqual(mock_backendProjectDetails.projectId)
+    })
+    
+    test('onClearBackend', () => {
+        // process.cwd = jest.fn(()=>{ return projectPath })
+
+        projectInfoManager.onClearBackend(mock_projectInfo)
+
+        expect(fs.writeFileSync).toBeCalled()
+        expect(fs.writeFileSync.mock.calls[0][0]).toBe(projectInfoFilePath)
+    })
+})
